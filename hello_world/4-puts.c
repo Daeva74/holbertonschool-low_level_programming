2cc5ccd6f9a8950aa6fb08178a95db02f80666b0
Daeva74 <9537@holbertonstudents.com>
2024-06-17 13:42:22 +0200

    hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.sh>
+#include<stdio.h>
 /**
- *main-entry point
+ *main - entry point
  *Return: Always 0 (success)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }
